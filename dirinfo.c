1d404a3f55a1a91ad4796430283a3259a31ede94
rleongPd5 <rleong00@stuy.edu>
2019-11-15 18:07:01 -0500

    COMPLETED ASSIGNMENT: Organized into printing directories vs regular files + total directory size based on size of each file in bytes

--- dirinfo.c
+++ dirinfo.c
@@ -26,9 +26,45 @@ int main(){
   }
   struct dirent *file;
   file = readdir(d);
+  struct stat buffer;
+  stat(".", &buffer);
+  if (errno < 0){
+    printf("ERROR in stat directory: %d: %s\n",errno, strerror);
+  }
+  printf("Statistics for directory:\n");
+  char reg[100];
+  char dir[100];
+  char s[100];
+  char c;
+  int bytes = 0;
+  strcpy(reg, ""); //makes sure that they are empty strings
+  strcpy(dir, ""); //for some reason reg and dir had weird chars and the beginning
   while (file){
-    printf("file: %s\n", file->d_name);
+    stat(file->d_name, &buffer);
+    bytes += buffer.st_size;
+    sprintf(s, "%o", buffer.st_mode);
+    // printf("%o\n", buffer.st_mode);
+    // printf("%s\n", s);
+    c = s[0];
+    // printf("%s\n", c);
+    if (c =='1'){ //regular
+      strcat(reg, "\t");
+      strcat(reg, file->d_name);
+      strcat(reg, "\n");
+      // printf("Added %s to reg\n",file->d_name );
+    }
+    else{
+      strcat(dir, "\t");
+      strcat(dir, file->d_name);
+      strcat(dir, "\n");
+    }
+    // printf("\t%s\n", file->d_name);
     file = readdir(d);
   }
+  printf("Total Directory Size: %d Bytes\n", bytes);
+  printf("Directories: \n");
+  printf("%s\n", dir);
+  printf("Regular files:\n");
+  printf("%s\n", reg);
   return 0;
 }
